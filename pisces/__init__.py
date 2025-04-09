54b09ed21949f6b69cdc63084df944a10af9160d
Eric Canton <eric.d.canton@gmail.com>
2025-04-09 15:12:36 -0500

    seems to have fixed imports. considering big refactor to rip out nbdev

--- pisces/__init__.py
+++ pisces/__init__.py
@@ -1,1 +1,11 @@
+"""Pisces: Sleep scoring tools, driven by examples.
+
+This package provides tools for sleep scoring using deep learning techniques, along with more classical techniques from statistical learning.
+"""
+from . import data_sets
+from . import deep_unet_support
+from . import metrics
+from . import utils
+from . import models
+
 __version__ = "2.0.0"
